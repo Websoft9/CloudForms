7f5bf547d39c1de004d9889a4e91edad5cb6cc20
qiaofeng1227 <76487013@qq.com>
2023-12-21 13:27:39 +0800

    Update start.sh

--- apps/activemq/cluster/start.sh
+++ apps/activemq/cluster/start.sh
@@ -6,3 +6,5 @@ sudo cp rclone-sync.timer /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable rclone-sync.service
 sudo systemctl enable rclone-sync.timer
+sudo systemctl start rclone-sync.service
+sudo systemctl start rclone-sync.timer
